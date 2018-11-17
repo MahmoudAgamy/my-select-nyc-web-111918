fcfdb43989354c121770a21535696433c303d460
Mahmoud Agamy <agamyv@gmail.com>
2018-11-17 01:12:28 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,7 +1,7 @@
 def my_select(collection)
  i = 0 
  while i < collection.length
-  if yield(collection[i])? collection[i]
+  collection[i] if yield(collection[i])
   i += 1
  end
  return collection
